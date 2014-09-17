6043b1127c5a052bd4dcf85dc9ae0cbab8021308
Daimeon DD Wren <daimeonddwren@gmail.com>
2014-09-17 13:26:22 -0600

    PokerPlayerDos

---
+++ PokePlayerDos/PokePlayerDos/PokerPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokePlayerDos
+{
+    class PokerPlayer
+    {
+        public List<Card> Hand { get; set; }
+        public PokerPlayer()
+        {
+            this.Hand = new List<Card>();
+            
+
+            
+
+
+        }
+
+    }
+}
---
+++ PokePlayerDos/PokePlayerDos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokePlayerDos
+{
+    public enum HandRank
+    {
+        HighCard,
+        OnePair,
+        TwoPairs,
+        ThreeofKind,
+        Straight,
+        Flush,
+        FullHouse,
+        FourofaKind,
+        StraightFlush,
+        RoyalFlush
+    }
+    class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            var deck = new Deck();
+            deck.PrintDeck();
+            deck.Shuffle();
+            Console.WriteLine("\nSHUFFLED\n");
+            deck.PrintDeck();
+
+           
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
